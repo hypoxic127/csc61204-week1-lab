545dbfda993f74e444d64777b08df5d4338cb374
hypoxic2 <aw1244936857@gmail.com>
2026-04-24 08:57:01 +0800

    123

---
+++ src/Week1/HelloMediTrack.java
@@ -0,0 +1,11 @@
+package Week1;
+
+public class HelloMediTrack {
+    public static void main(String[] args) {// Sprint 0 verification - confirms environment is working
+        System.out.println("MediTrack Patient System - v0.1");
+        System.out.println("Developer: hypoxi");
+        System.out.println("Student ID: 12345");
+        System.out.println("Status: Environment configured successfully.");
+
+    }
+}
