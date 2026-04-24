cfb9456038d20ad4c0a19bc73511efee98b9a7e8
hypoxic2 <aw1244936857@gmail.com>
2026-04-24 09:02:55 +0800

    Task 2: HelloMediTrack complete

--- src/Week_1/HelloMediTrack.java
+++ src/Week_1/HelloMediTrack.java
@@ -1,4 +1,6 @@
-package Week1;
+package Week_1;
+
+import java.time.LocalDate;
 
 public class HelloMediTrack {
     public static void main(String[] args) {// Sprint 0 verification - confirms environment is working
@@ -6,6 +8,7 @@ public class HelloMediTrack {
         System.out.println("Developer: hypoxi");
         System.out.println("Student ID: 12345");
         System.out.println("Status: Environment configured successfully.");
-
+        LocalDate date = LocalDate.now();
+        System.out.println("Welcome to MediTrack, hypoxi. Today is " + date + ".");
     }
 }
